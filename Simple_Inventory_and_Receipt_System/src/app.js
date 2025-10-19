2adbf15161f40136ca43a2245c6f8073490a3a2a
MarkRyuo <malupajhonmark3035@gmail.com>
2025-10-19 15:17:06 +0800

    Simple_Inventory_and_Receipt_System: Update

--- Simple_Inventory_and_Receipt_System/src/app.js
+++ Simple_Inventory_and_Receipt_System/src/app.js
@@ -1,55 +1,54 @@
 import { prices, products } from "../data.js";
 
 /**
- * May list ng products (pang-practice ng arrays). 
+ * May list ng products (pang-practice ng arrays).
  * Kayang mag-compute ng total, tax, discount (pang-practice ng arithmetic).
  * May customer name na lalabas sa final message (pang-practice ng strings).
  * Gumagamit ng variables, assignment, at const/let.
  * Magpapakita ng receipt gamit string concatenation at escape characters.
  */
 
-
 // total, tax, discount
 
-
 function compute() {
-    let subTotal = 0; 
-    for(let i = 0; i < prices.length; i++){
-            subTotal += prices[i];
-}
-console.log(subTotal)
-// What is your name:  ?
-const customerName = document.getElementById("customerName");
-const value = customerName.value.trim();
+  let subTotal = 0;
+  for (let i = 0; i < prices.length; i++) {
+    subTotal += prices[i];
+  }
 
-// Get the tax ;
-const tax = 0.12 ;
+  console.log(subTotal);
+  // What is your name:  ?
+  const customerName = document.getElementById("customerName");
+  const value = customerName.value.trim();
 
-// Get the discount 
-const discount = 0.10 ;
+  // Get the tax ;
+  const tax = 0.12;
 
-// compute
+  // Get the discount
+  const discount = 0.1;
 
-const salesTaxAmount = (tax / 100) * subTotal ;
-const totalDiscount = discount * 100 ;
-const total = subTotal + salesTaxAmount - totalDiscount;
+  // compute
 
-const receipt = document.getElementById("receipt");
+  const salesTaxAmount = (tax / 100) * subTotal;
+  const totalDiscount = discount * 100;
+  const total = subTotal + salesTaxAmount - totalDiscount;
 
-let arrProduct = "";
+  const receipt = document.getElementById("receipt");
 
-receipt.textContent = `
-    Hello Customer! \n 
-    Name: ${value} \n
+  let arrProduct = "";
+
+  receipt.textContent = `
+    Hello Customer!
+    Name: ${value} 
     Products = ${products.join(arrProduct)}
     Total: ${total}
     tax: VAT ${tax}
     discount: ${discount.toFixed(2)}%
 
-`
+`;
 
-console.log(receipt)
+  console.log(receipt);
 }
 
 const btnSubmit = document.getElementById("btnSubmit");
-btnSubmit.addEventListener("click", () => compute())
+btnSubmit.addEventListener("click", () => compute());
