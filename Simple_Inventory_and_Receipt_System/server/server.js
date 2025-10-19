fcf202c6248cde5c2e795f8fa522c291a43a2002
MarkRyuo <malupajhonmark3035@gmail.com>
2025-10-19 17:42:07 +0800

    Simple_Inventory_and_Receipt_System: Update

--- Simple_Inventory_and_Receipt_System/server/server.js
+++ Simple_Inventory_and_Receipt_System/server/server.js
@@ -1,7 +1,12 @@
 // server 
 const express = require('express');
 const app = express() ;
+const port = 3000 ;
 
 app.get('/', (request, response)=> {
-    
+    response.send("Hello, World!")
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Example app listening on port ${port}`)
 })
