20c0e5db4bb55ec354b601b915ef8cadb5a27c89
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-10-19 08:52:23 +0800

    SalesInventorandReceiptSyste: Update

--- Simple_Inventory_and_Receipt_System/data.js
+++ Simple_Inventory_and_Receipt_System/data.js
@@ -1,22 +1,6 @@
 
-const listOfProduct = [
-    {
-        beefLoof: [
-            {item: "Argentina beef loof 150g", price: 22.00},
-            {item: "Argentina beef loof 100g", price: 16.00},
-            {item: "Lami beef loof big", price: 35.00},
-            {item: "Lami beef loof 150g", price: 23.00},
-            {item: "Lami beef loof 100g", price: 18.10},
-        ], 
-        tuna: [
-            {item: "555 Tuna junior", price: 24.00},
-            {item: "555 Tuna 150g", price: 27.00},
-            {item: "Century Tuna", price: 35.00},
-            {item: "Fresca Tuna", price: 27.00},
-            {item: "Wow Ulam", price: 21.00},
-        ]
-    }, 
-];
 
-export default listOfProduct ;
+export const products = ["Wireless Mouse", "Mechanical Keyboard", "HD Monitor", "USB Hub", "Desk Lamp"];
+export const prices = [100.00, 500.00, 1200.00, 300.00, 200.00];
+
 
--- Simple_Inventory_and_Receipt_System/src/app.js
+++ Simple_Inventory_and_Receipt_System/src/app.js
@@ -1,4 +1,4 @@
-import listOfProduct from "../data.js";
+import { prices, products } from "../data.js";
 
 /**
  * May list ng products (pang-practice ng arrays). 
@@ -11,70 +11,45 @@ import listOfProduct from "../data.js";
 
 // total, tax, discount
 
-// sa discount mag base tayo sa age;
 
-const inputAge = () => {
-    const customerAge = document.getElementById("customerAge");
-    const ageValue = Number(customerAge.value) ;
+function compute() {
+    let subTotal = 0; 
+    for(let i = 0; i < prices.length; i++){
+            subTotal += prices[i];
+}
+console.log(subTotal)
+// What is your name:  ?
+const customerName = document.getElementById("customerName");
+const value = customerName.value.trim();
 
-    return ageValue ;
+// Get the tax ;
+const tax = 0.12 ;
+
+// Get the discount 
+const discount = 0.10 ;
+
+// compute
+
+const salesTaxAmount = (tax / 100) * subTotal ;
+const totalDiscount = discount * 100 ;
+const total = subTotal + salesTaxAmount - totalDiscount;
+
+const receipt = document.getElementById("receipt");
+
+let arrProduct = "";
+
+receipt.textContent = `
+    Hello Customer! \n 
+    Name: ${value} \n
+    Products = ${products.join(arrProduct)}
+    Total: ${total}
+    tax: VAT ${tax}
+    discount: ${discount.toFixed(2)}%
+
+`
+
+console.log(receipt)
 }
 
-
-// discount for age of <18 = 2%, age >== 18 to 35 = 5%, age < 35 to 59 = 7%, age > 59 to up = 10%
-const baseAge = (age) => {
-    let discount ;
-
-    if(age < 18){
-        console.log("Your less 18")
-        return discount = 2 ;
-    } else if(age >= 18 || age >= 35){
-        console.log("Your 18 above")
-        return discount = 5 ;
-    } else if(age < 35 || age <= 59){
-        console.log("Your 35 above")
-        return discount = 7 ;
-    } else if(age > 59){
-        console.log("Your 60 above")
-        return discount = 10
-    }
-};
-
-const displayProducts = () => {
-    
-
-}
-
-
-
-
-// Main
-function computed(){
-    let total ;
-    const tax = 12 ; // fixed tax by 12%
-
-    const customerName = document.getElementById("customerName");
-    const listOfBeefLoof = document.getElementById("listOfBeefLoof");
-    const listOfTuna = document.getElementById("listOfTuna");
-
-    const beefLoof = listOfProduct[0].beefLoof ;
-    const tuna = listOfProduct[0].tuna;
-
-    beefLoof.forEach((items) => {
-        const li = document.createElement("li");
-        li.textContent = `${items.item} - ₱${items.price.toFixed(2)}`
-        listOfBeefLoof.appendChild(li)
-    })
-
-    tuna.forEach((tuna) => {
-        const li = document.createElement("li");
-        li.textContent = `${tuna.item} - ₱${tuna.price.toFixed(2)}`;
-        listOfTuna.appendChild(li);
-    })
-
-    const inputAges = inputAge();
-
-
-}; 
-
-computed()
+const btnSubmit = document.getElementById("btnSubmit");
+btnSubmit.addEventListener("click", () => compute())
