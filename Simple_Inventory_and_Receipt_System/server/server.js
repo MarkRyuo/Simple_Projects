33d084146cf5698b2457b872fc45ebbd5d42db14
MarkRyuo <malupajhonmark3035@gmail.com>
2025-10-20 14:51:38 +0800

    Simple_Inventory_and_Receipt_System: Update

--- Simple_Inventory_and_Receipt_System/server/server.js
+++ Simple_Inventory_and_Receipt_System/server/server.js
@@ -1,14 +1,22 @@
-// server 
 const express = require('express');
-const app = express() ;
-const port = 3000 ;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-app.get('/', (request, response)=> {
-    response.send("Hello, World!")
-})
+dotenv.config();
 
-app.listen(port, (req, res) => {
-    console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+const port = 3000;
 
-// http://localhost:3000
+app.get('/', (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+mongoose.connect(
+  `mongodb+srv://${process.env.USER_NAME_DB}:${process.env.USER_PASSWORD_DB}@sampledb.devtvnm.mongodb.net/?retryWrites=true&w=majority&appName=sampleDb`
+)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch(err => console.error('MongoDB connection error:', err));
